e0cbb3db170dd37b4df94e4a86b933de73a4f929
conghuahuadan <conghuahuadan@163.com>
2020-04-30 01:06:08 +0800

    测试Glide加载图片

--- app/src/main/java/com/chhd/superlayout/demo/MainActivity.java
+++ app/src/main/java/com/chhd/superlayout/demo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.chhd.superlayout.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView iv = findViewById(R.id.iv);
+        Glide.with(iv).load("http://i0.hdslb.com/bfs/article/e64ed895471547ecb86e91e1d287993cbeb8377a.jpg").into(iv);
     }
 }
